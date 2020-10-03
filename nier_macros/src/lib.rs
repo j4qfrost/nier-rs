1b6c0eee909e15529faf0a00ecfc2989f72926c2
Jack Frost <j4qfrost@gmail.com>
2020-10-03 01:28:47 +0000

    macro doesnt work yet

--- nier_macros/src/lib.rs
+++ nier_macros/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(format_args_capture)]
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
@@ -34,47 +35,109 @@ pub fn derive_alphabet(input: TokenStream) -> TokenStream {
 
 // Some ideas
 
-// use ron::de::from_reader;
-// use serde::Deserialize;
+use ron::de::from_reader;
+use serde::Deserialize;
+use std::fs::File;
+use std::env;
 
-// use nier::*;
 
-// fn main() {
-//     let input_path = format!("{}/examples/example.ron", env!("CARGO_MANIFEST_DIR"));
-//     let f = File::open(&input_path).expect("Failed opening file");
-//     let config: Config = match from_reader(f) {
-//         Ok(x) => x,
-//         Err(e) => {
-//             println!("Failed to load config: {}", e);
+#[derive(Debug, Deserialize)]
+struct IntermediateDFA {
+    name: String,
+    state_name: String,
+    states: Vec<String>,
+    alphabet_name: String,
+    alphabet: Vec<String>,
+    initial_state: String,
+    transitions: std::collections::HashSet<(String, String, String)>,
+    accept_states: Vec<String>,
+}
 
-//             std::process::exit(1);
-//         }
-//     };
+#[proc_macro]
+pub fn deserialize_dfa(args: TokenStream) -> TokenStream {
+    let strip = args.to_string().replace(" ", "");
+    let file_name = strip.as_str();
 
-//     println!("Config: {:?}", &config);
-// }
+    let input_path = format!("{}/{}", env::current_dir().unwrap().to_str().unwrap(), file_name);
+    let f = File::open(&input_path).expect("Failed opening file");
+    let IntermediateDFA {
+            name,
+            state_name,
+            states,
+            alphabet_name,
+            alphabet,
+            initial_state,
+            transitions,
+            accept_states,
+        } = match from_reader(f) {
+        Ok(x) => x,
+        Err(e) => {
+            println!("Failed to load config: {}", e);
 
-// #[proc_macro_attribute]
-// pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
-//     println!("attr: \"{}\"", attr.to_string());
-//     println!("item: \"{}\"", item.to_string());
-//     item
-// }
+            std::process::exit(1);
+        }
+    };
 
-// use seq::seq;
+    let mut transformed_states = "".to_string();
+    for s in states {
+        transformed_states.push_str(&format!("{},", s));
+    }
+    let mut transformed_alphabet = "".to_string();
+    for a in alphabet {
+        transformed_alphabet.push_str(&format!("{},", a));
+    }
 
-// seq!(N in 0..512 {
-//     #[derive(Copy, Clone, PartialEq, Debug)]
-//     pub enum Processor {
-//         #(
-//             Cpu#N,
-//         )*
-//     }
-// });
+    let mut transformed_transitions = "".to_string();
+    for (state, input, pstate) in &transitions {
+        transformed_transitions.push_str(&format!("({state_name}::{state}, {alphabet_name}::{input}) => Ok({state_name}::{pstate}),"));
+    }
+    let mut transformed_accept_states = "".to_string();
+    for state in &accept_states {
+        transformed_accept_states.push_str(&format!("{state_name}::{state} => Ok({state_name}::{state}),"));
+    }
 
-// fn main() {
-//     let cpu = Processor::Cpu8;
+    let expanded = quote! {
+        use std::hash::Hash;
 
-//     assert_eq!(cpu as u8, 8);
-//     assert_eq!(cpu, Processor::Cpu8);
-// }
+        struct r#name {}
+
+        #[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
+        enum r#state_name {
+            r#transformed_states
+        }
+
+        #[derive(Debug, Copy, Clone)]
+        enum r#alphabet_name {
+            r#transformed_alphabet
+        }
+
+        impl Automaton<r#state_name> for r#name {}
+
+        impl Deterministic<r#state_name, r#alphabet_name> for r#name {
+            fn initial() -> r#state_name {
+                r#state_name::r#initial_state
+            }
+
+            fn delta(
+                state: &r#state_name,
+                input: r#alphabet_name,
+            ) -> Result<r#state_name, Reject<r#state_name, r#alphabet_name>> {
+                match (state, input) {
+                    r#transformed_transitions
+                    _ => Err(Reject::InvalidInput(input.clone())),
+                }
+            }
+        }
+
+        impl Acceptor<#state_name> for r#name {
+            fn accept(current: &r#state_name) -> Result<r#state_name, Reject<r#state_name, ()>> {
+                match current {
+                    r#transformed_accept_states
+                    _ => Err(Reject::NotAccept(current.clone())),
+                }
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
